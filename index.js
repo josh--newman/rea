9320de7c29e686c5f467473788b45fde950384a8
Josh Newman <josh.newman4390@gmail.com>
2017-11-05 20:22:13 +1100

    Get file path from command line

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { parseFileToActions } = require('./src/parser');
 const { calculateState } = require('./src/reducers');
 
-const actions = parseFileToActions('./src/__tests__/examples/exampleA.txt');
+const filePath = process.argv[2];
+if (!filePath) {
+  console.error('ERROR: Please provide a file path.');
+  process.exit(1);
+}
+const actions = parseFileToActions(filePath);
 calculateState(actions); // A REPORT action will internally log state
 process.exit(0);
